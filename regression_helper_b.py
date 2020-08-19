22ee14ca2df959c914fb5a3eae6efa3fce19c2a8
Julian <jganzabal@infoxel.com>
2020-08-19 18:00:55 -0300

    first commit

--- regression_helper_b.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-
-def make_regression(file):
-    df = pd.read_csv(file)
-    lr = LinearRegression()
-    lr.fit(df['Peso'].values.reshape(-1, 1), df['Altura'].values.reshape(-1, 1))
-    return lr.coef_[[0]], lr.intercept_[0]
-    
